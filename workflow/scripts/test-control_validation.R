d3c7e62c18edd3f3e2f45f907e1b59d2b49b2245
Lightning Auriga <lightning.auriga@gmail.com>
2023-12-15 13:34:49 -0500

    test: add placeholders for eventual tests

--- workflow/scripts/test-control_validation.R
+++ workflow/scripts/test-control_validation.R
@@ -6,3 +6,27 @@ if (!isClass("snakemake.s4")) {
 }
 
 source("control_validation.R")
+
+test_that("load.single.file successfully reformats an input csv file in tidy format", {
+
+})
+
+test_that("load.files aggregates and subsets a set of csv files", {
+
+})
+
+test_that("construct.targets converts flattened input configuration data into a usable format", {
+
+})
+
+test_that("filter.targets removes datasets with no content", {
+
+})
+
+test_that("make.plot creates a plot with consistent structure", {
+
+})
+
+test_that("make.table creates a table with consistent structure", {
+
+})
